6103cba3d09e89291a600bb5efc3a0e5658e352a
Lewis Bock <lewis.bock@gmail.com>
2025-12-18 07:42:29 +0100

    feat: updated mixin files with import variable + All section visible

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Header from './components/Header/Header.jsx';
-// import Hero from './sections/Hero/Hero.jsx';
-// import About from './sections/About/About.jsx';
-// import Skills from './sections/Skills/Skills.jsx';       
-// import Projects from './sections/Projects/Projects.jsx';
-// import Contact from './sections/Contact/Contact.jsx';
-// import Footer from './components/Footer/Footer.jsx';
+import Hero from './sections/Hero/Hero.jsx';
+import About from './sections/About/About.jsx';
+import Skills from './sections/Skills/Skills.jsx';       
+import Projects from './sections/Projects/Projects.jsx';
+import Contact from './sections/Contact/Contact.jsx';
+import Footer from './components/Footer/Footer.jsx';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
@@ -13,14 +13,14 @@ function App() {
       <Header />
       
       <main>
-        {/* <Hero /> */}
-        {/* <About /> */}
-        {/* <Skills /> */}
-        {/* <Projects /> */}
-        {/* <Contact /> */}
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
       </main>
       
-      {/* <Footer /> */}
+      <Footer />
       <ScrollToTop />
     </>
   );
